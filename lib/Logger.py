eb9a93562befed9cd342d343802429ca924c033f
Aynur <ainur2205@gmail.com>
2024-10-15 21:32:55 +0500

    промежуточные изменения: методы для логгера

--- lib/Logger.py
+++ lib/Logger.py
@@ -4,3 +4,14 @@ class Logger:
 
     def __init__(self):
         print('1')
+
+    """
+    Метод записывающий собщения в файл
+    """
+    def record_to_file(self):
+        pass
+    """
+    Метод для очистки существующего log файла.
+    """
+    def clear_last_log(self):
+        pass
