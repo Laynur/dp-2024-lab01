ed497433b82d86dd9e8fe88be6b22d6a0721b047
Aynur <ainur2205@gmail.com>
2024-10-24 20:31:33 +0500

    промежуточные изменения

--- lib/Logger.py
+++ lib/Logger.py
@@ -1,35 +1,38 @@
 from datetime import datetime
+import threading
+import os
 
 
 class Logger:
 
+    _lock = threading.Lock()
+
     def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(Logger, cls).__new__(cls)
-            cls.instance._create_log()
+        with cls._lock:
+            if not hasattr(cls, 'instance'):
+                cls.instance = super(Logger, cls).__new__(cls)
+                cls.instance._create_log()
         return cls.instance
 
     """
         Метод для создания нового файла с логами
     """
     def _create_log(self):
-        now = datetime.now()
-        self.filename = now.strftime("DP.P1.%Y-%m-%d.%H-%M-%S.log")
-        with open(self.filename, 'w'):
-            pass
+        time_now = datetime.now().strftime('%Y-%m-%d.%H-%M-%S')
+        log_file_name = f"DP.P1.{time_now}.log"
+        self.log_file_path = os.path.join(os.getcwd(), log_file_name)
 
     """
-      Метод записывающий собщения в файл
+        Метод записывающий собщения в файл
     """
-    def record_to_file(self, level: str, message: str):
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        with open(self.filename, 'a') as log_file:
-            log_file.write(f"{now} [{level}] {message}\n")
-    """
-    Метод для очистки существующего log файла.
-    """
-    def clear_last_log(self):
-        pass
+    def record_log_to_file(self, level: str, message: str):
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        log_message = f"{timestamp} [{level}] {message}\n"
+
+        with self._lock:
+            with open(self.log_file_path, 'a') as log_file:
+                log_file.write(log_message)
+
 
 class Singleton(object):
     def __new__(cls):
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from lib.Logger import Logger, Singleton
 import threading
-log = Logger()
-log.record_to_file("INFO", "Приложение запущено")
 
-# s = Singleton()
-# print(s.instance)
-# print("Создан объект",s)
-# s1 = Singleton()
-# print("Создан объект",s1)
+
+def worker(level: str, message: str):
+    log = Logger()
+    print(log)
+    log.record_log_to_file(level, message)
+
+
+for i in range(10000):
+    th = threading.Thread(worker('INFO',f'Поток {i}'))
+    th.start()
