908e12ad96e7fccb858e933494bfb34590e818cb
Aynur <ainur2205@gmail.com>
2024-10-28 19:46:06 +0500

    fix(Logger): Исправлены недочеты, переименованы методы

--- lib/Logger.py
+++ lib/Logger.py
@@ -1,41 +1,34 @@
 from datetime import datetime
+from lib.LevelMessage import LevelMessage
 import threading
 import os
 
 
 class Logger:
-
     _lock = threading.Lock()
 
     def __new__(cls):
         with cls._lock:
             if not hasattr(cls, 'instance'):
                 cls.instance = super(Logger, cls).__new__(cls)
-                cls.instance._create_log()
+                cls.instance._create_file_path()
         return cls.instance
 
-    """
+    def _create_file_path(self):
+        """
         Метод для создания нового файла с логами
-    """
-    def _create_log(self):
+        """
         time_now = datetime.now().strftime('%Y-%m-%d.%H-%M-%S')
         log_file_name = f"DP.P1.{time_now}.log"
         self.log_file_path = os.path.join(os.getcwd(), log_file_name)
 
-    """
+    def log(self, level: str, message: str):
+        """
         Метод записывающий собщения в файл
-    """
-    def record_log_to_file(self, level: str, message: str):
+        """
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         log_message = f"{timestamp} [{level}] {message}\n"
 
         with self._lock:
             with open(self.log_file_path, 'a') as log_file:
                 log_file.write(log_message)
-
-
-class Singleton(object):
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(Singleton, cls).__new__(cls)
-        return cls.instance
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from lib.Logger import Logger, Singleton
+from lib.Logger import Logger
+from lib.LevelMessage import LevelMessage
 import threading
 
 
 def worker(level: str, message: str):
-    log = Logger()
-    print(log)
-    log.record_log_to_file(level, message)
+    logger = Logger()
+    print(logger)
+    logger.log(level.value, message)
 
 
-for i in range(10000):
-    th = threading.Thread(worker('INFO',f'Поток {i}'))
+for i in range(10):
+    th = threading.Thread(target=worker, args=(LevelMessage.INFO, f'Поток {i}'))
     th.start()
