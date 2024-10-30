b0ecbfc159653425e1c80f92cb7627cc276d2825
Aynur <ainur2205@gmail.com>
2024-10-30 19:48:03 +0500

    chore(Logger): Исправлены недочеты. Добавил 2 лок, для записи метода log и для создание экземпляра класса. Добавил новый метод для формирования строки лога

--- lib/Logger.py
+++ lib/Logger.py
@@ -1,33 +1,35 @@
 from datetime import datetime
+from lib.LevelMessage import LevelMessage
 import threading
 import os
 
 
 class Logger:
-    _lock = threading.Lock()
+    _lock_write = threading.Lock()
+    _lock_create = threading.Lock()
 
     def __new__(cls):
-        with cls._lock:
+        with cls._lock_create:
             if not hasattr(cls, 'instance'):
                 cls.instance = super(Logger, cls).__new__(cls)
                 cls.instance._create_file_path()
         return cls.instance
 
     def _create_file_path(self):
-        """
-        Метод для создания пути к файлу журнала
-        """
+        """Метод для создания пути к файлу журнала"""
         time_now = datetime.now().strftime('%Y-%m-%d.%H-%M-%S')
         log_file_name = f"DP.P1.{time_now}.log"
         self.log_file_path = os.path.join(os.getcwd(), log_file_name)
 
-    def log(self, level: str, message: str):
-        """
-        Метод записывающий собщения в файл
-        """
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        log_message = f"{timestamp} [{level}] {message}\n"
+    def log(self, level: LevelMessage, message: str):
+        """Метод записывающий собщения в файл"""
 
-        with self._lock:
+        with self._lock_write:
+            log_message = self._create_message(message=message, level=level)
             with open(self.log_file_path, 'a') as log_file:
                 log_file.write(log_message)
+
+    def _create_message(self, message: str, level: LevelMessage):
+        """Метод для формирования строки лога"""
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        return f"{timestamp} [{level}] {message}\n"
